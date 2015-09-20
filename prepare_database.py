73135e6476cc44fd89ea9fa784848bd06314c21f
zhangjialin <cainiaocome@gmail.com>
2015-09-20 20:18:58 +0800

    init

---
+++ prepare_database.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python2.7
+# encoding: utf-8
+
+import pymysql as mdb
+
+DB_HOST = '127.0.0.1'
+DB_USER = 'root'
+DB_PASS = 'jialin,0204'
+
+create_table_search_filelist = '''
+create table if not exists search_filelist
+(
+    info_hash varchar(40),
+    file_list text,
+    primary key (info_hash)
+)
+'''
+create_table_search_hash = '''
+create table if not exists search_hash
+(
+    id int unsigned AUTO_INCREMENT,
+    info_hash varchar(40) unique,
+    category varchar(20),
+    date_hash varchar(32),
+    name varchar(255),
+    extension varchar(20),
+    classified boolean default 0,
+    source_ip varchar(20),
+    tagged boolean default 0,
+    length bigint unsigned,
+    create_time date,
+    last_seen date,
+    requests int unsigned,
+    comment varchar(255),
+    creator varchar(20),
+    primary key (id)
+)
+'''
+create_table_search_statusreport = '''
+create table if not exists search_statusreport
+(
+    date date unique,
+    new_hashes int,
+    total_requests int,
+    valid_requests int
+)
+'''
+
+print create_table_search_filelist
+
+dbconn = mdb.connect(DB_HOST, DB_USER, DB_PASS, 'ssbc', charset='utf8')
+dbconn.autocommit(False)
+dbcurr = dbconn.cursor()
+dbcurr.execute('SET NAMES utf8')
+
+dbcurr.execute(create_table_search_filelist)
+dbconn.commit()
+dbcurr.execute(create_table_search_hash)
+dbconn.commit()
+dbcurr.execute(create_table_search_statusreport)
+dbconn.commit()
+dbconn.close()
